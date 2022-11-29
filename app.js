0dcda1cce7ed3cb510823c2933524038d6d9c4cf
Jordan McKinney <jordanmmck@gmail.com>
2022-11-29 14:47:41 -0800

    init commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { Client } = require("pg");
+
+var connectionString = "postgres://postgres:postgres@localhost:5432/jordan";
+const client = new Client({
+  connectionString: connectionString,
+});
+client.connect();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("server is working...");
+});
+
+app.get("/get-from-db", (req, res) => {
+  client.query("SELECT * FROM data", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send({
+        data: result.rows,
+      });
+    }
+  });
+});
+
+app.get("/math-test", (req, res) => {
+  const limit = 10 * 10 ** 8;
+
+  let numerator = 0;
+  let denominator = 1;
+  let result = 0;
+
+  for (let i = 0; i < limit; i++) {
+    result += numerator++ / denominator++;
+  }
+  res.send({ value: result });
+});
+
+app.get("/get-from-api", (req, res) => {
+  fetch("https://httpbin.org/get")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      res.send({ data: data });
+    });
+});
+
+app.listen("3000");
---
+++ readme.md
@@ -0,0 +1,24 @@
+# simple js server
+
+install and run postgres
+
+## setup
+
+install postresql and create a table in a database with a single integer in it. for example:
+
+```psql
+CREATE TABLE data(a int);
+INSERT INTO data(a) VALUES (999)
+```
+
+edit your connection string (in app.js) to match your db name. ie. replace "jordan" with your db name. 
+
+run the server:
+
+```bash
+node index.js
+```
+
+## usage
+
+hit the endpoints from insomnia/postman/browser
